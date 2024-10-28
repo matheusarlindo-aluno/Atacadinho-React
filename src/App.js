90f073e879485ace32e7a8d9ccdd57d5aebe810b
Matheus Arlindo Silva de Jesus <2301778@SALAS.AULAS>
2024-10-28 09:36:20 -0300

    sim

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import FormUser from './components/FormUser';
+import ListarUsuario from './components/ListarUsuario';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FormUser from './components/FormUser';
+import ListarUsuario from './components/ListarUsuario';
+import CadastroUser from './CadastroUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App></App>}></Route>
-        <Route path='/create-user' element={<FormUser></FormUser>}></Route>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/save' element={<CadastroUser/>}></Route>
+        <Route path='/listar-user' element={<ListarUsuario/>}></Route>
       </Routes>
     </BrowserRouter>
     
   </React.StrictMode>
 );
-
 
 reportWebVitals();
